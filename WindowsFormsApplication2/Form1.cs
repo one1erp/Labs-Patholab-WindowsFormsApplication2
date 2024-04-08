fbed65e27edff7a4a1fb480c424fa121c28e9f82
one1erp <avigaile@one1.co.il>
2024-04-08 17:34:44 +0300

    Initial commit with additional files

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FAXCOMLib;
+using System.Windows.
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            richTextBox1.d
+            
+            //FaxServer sever = new FaxServer();
+            //FaxDoc doc = null;
+            //try
+            //{
+            //    sever.Connect(Environment.MachineName);
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+            //sever.Disconnect();
+        }
+    }
+}
